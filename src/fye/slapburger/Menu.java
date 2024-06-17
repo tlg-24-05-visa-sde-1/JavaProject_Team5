66ef8c7bb865205027b7effd9609077b86dcbb8a
Olutayo Fabuluje <olutayof@tlgcohort.com>
2024-06-17 14:29:05 -0500

    Updated placeOrder() in BigBackBurgerApp

---
+++ src/fye/slapburger/Menu.java
@@ -0,0 +1,16 @@
+package fye.slapburger;
+
+import java.util.List;
+
+public class Menu {
+    private List<MenuItem> menuItems;
+
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+}
