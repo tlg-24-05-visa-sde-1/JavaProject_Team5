1f948ce08c4e708ab09f9ca0f671dcca7da5b200
Douglas Sigmon <douglass@tlgcohort.com>
2024-06-17 10:37:11 -0500

    added main

--- src/fye/slapburger/app/BigBackBurgerApp.java
+++ src/fye/slapburger/app/BigBackBurgerApp.java
@@ -3,13 +3,9 @@ package fye.slapburger.app;
 import fye.slapburger.FoodCategory;
 import fye.slapburger.FoodTruck;
 
-class BigBackBurgerApp {
+public class BigBackBurgerApp {
   FoodTruck foodTruck = new FoodTruck();
 
-  public static void main(String[] args) {
-
-
-
+  public void execute() {
   }
-
 }
---
+++ src/fye/slapburger/client/Main.java
@@ -0,0 +1,12 @@
+package fye.slapburger.client;
+
+import fye.slapburger.app.BigBackBurgerApp;
+
+class Main {
+
+  public static void main(String[] args) {
+    BigBackBurgerApp app = new BigBackBurgerApp();
+    app.execute();
+  }
+
+}
