0e1454bbcdada8bccf2b57f3d8ab91fcf3021a43
Douglas Sigmon <douglass@tlgcohort.com>
2024-06-19 09:01:23 -0500

    added splash screen

--- src/fye/slapburger/client/Main.java
+++ src/fye/slapburger/client/Main.java
@@ -3,11 +3,17 @@ package fye.slapburger.client;
 import com.apps.util.SplashApp;
 import fye.slapburger.app.BigBackBurgerApp;
 
-class Main  {
+class Main implements SplashApp {
 
   public static void main(String[] args) {
+    Main mainApp = new Main();
+    mainApp.welcome("images/steakfries.png", "images/truck.png");
+    mainApp.start();
+  }
+
+  @Override
+  public void start() {
     BigBackBurgerApp app = new BigBackBurgerApp();
     app.execute();
-
   }
 }
